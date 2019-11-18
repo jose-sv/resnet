a349034646b5c1dd6778fc13b17ea70713d779e8
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2019-11-18 13:56:04 -0600

    Add a dropout layer

--- models/resnet.py
+++ models/resnet.py
@@ -84,6 +84,7 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.dropout = nn.Dropout(p=0.3)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -99,6 +100,7 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
+        out = self.dropout(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
